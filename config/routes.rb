6c05787ab0998bf4ee805396f65932811a873132
Jair Escamilla <hernandez_yo14@outlook.es>
2019-06-18 22:46:53 -0500

    Comenzando a almacenar en la BD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenida", to: "home#index"
+
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  
 end
