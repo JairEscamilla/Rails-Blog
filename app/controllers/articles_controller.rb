24979e5dfbc970f710225aebb2d202a696a8ce8e
Jair Escamilla <hernandez_yo14@outlook.es>
2019-06-19 00:20:58 -0500

    Editando articulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,20 @@ class ArticlesController < ApplicationController
     def show
        @article = Article.find(params[:id]) 
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        
+        redirect_to @article
+    end
+
     def new
         @article = Article.new
-        @article.title = "Demo"
     end
 
     def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
   get "articles/:id", to: "articles#show"
-
+  get "articles/:id/edit", to: "articles#edit"
+  patch "/articles/:id", to: "articles#update", as: :article
 end
