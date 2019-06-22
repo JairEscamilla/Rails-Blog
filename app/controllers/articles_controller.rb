c8c95d2e57efee8790b1f11cea735296c2b945d2
Jair Escamilla <hernandez_yo14@outlook.es>
2019-06-21 22:46:38 -0500

    Mejorando codigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit, :destroy]
     def show
-       @article = Article.find(params[:id]) 
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         @article.update(title: params[:article][:title], content: params[:article][:content])
         
         redirect_to @article
@@ -24,8 +22,12 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
     end
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
 end
