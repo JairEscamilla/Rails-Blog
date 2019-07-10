99b204f022449a8567862ab4c836848ad2430d4b
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-09 22:57:48 -0500

    Relaciones

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
     has_rich_text :content
     belongs_to :user # Campo adicional    
+    has_many :has_categories
+    has_many :categories, through: :has_categories
 end
