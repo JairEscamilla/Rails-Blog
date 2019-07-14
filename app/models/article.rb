cb3bc4d489e8915c03fb55d85b0548fb589bd859
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-14 14:53:12 -0500

    Mostrando los tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
 
     def save_categories
         #categories_array = category_elements.split(",")
-        categories_array.each do |category_id|
+        category_elements.each do |category_id|
             # HasCategory.find_or_create_by(article: self, category_id: category_id)
             unless HasCategory.where(article: self, category_id: category_id).any?
                 HasCategory.create(article: self, category_id: category_id)
