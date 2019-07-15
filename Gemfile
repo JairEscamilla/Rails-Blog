26b0cbb4aaff867592476e2b856a01509ffa2420
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-15 16:41:28 -0500

    Prob

--- Gemfile
+++ Gemfile
@@ -54,8 +54,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
-  gem 'rails_12factor', "0.0.2"
+  gem 'pg'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,10 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
