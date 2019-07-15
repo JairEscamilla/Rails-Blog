aecf53416465122e6bcfccdb2b8c5b326432c1c0
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-15 17:28:05 -0500

    Prob

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.}
     #config.assets.initialize_on_precompile = false
-    #config.load_defaults 6.0
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
