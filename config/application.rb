60d3ca5ec95eddbc39242ee1db6db1df9bca1e2d
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-15 17:22:14 -0500

    Espero que ya funcione

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.}
-    config.assets.initialize_on_precompile = false
-    config.load_defaults 6.0
+    #config.assets.initialize_on_precompile = false
+    #config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
