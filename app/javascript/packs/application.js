d3aa2a9f73bb937ba03aadf32bada2653482ac01
Jair Escamilla <hernandez_yo14@outlook.es>
2019-07-15 19:10:11 -0500

    Css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap'
-import 'css/styles'
+//import './assets/stylesheets/styles'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,4 +19,3 @@ import 'css/styles'
 
 require("trix")
 require("@rails/actiontext")
-alert("Hola");
